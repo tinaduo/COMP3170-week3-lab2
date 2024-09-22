1049f1c921fbef9a1e273575b7bba66cedc103d4
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-09-22 16:02:49 -0700

    made heading component

---
+++ components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Heading() {
+    return (
+    <h1>best selling music artists</h1>
+    );
+}
+
+export default Heading;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { artists } from './best-selling-music-artists';
 import ArtistDetails from '/components/ArtistDetails';
+import Heading from '../components/Heading';
 
 function App() {
   return (
     <div className="app">
-      <h1>best selling music artists</h1>
+      <Heading/>
       <div className="artist-list">
         {artists.map((artist, index) => (
           <ArtistDetails key={index} artist={artist} />
